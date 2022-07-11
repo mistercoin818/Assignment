74b2d9ff54d96c80c7e818e58a6db9dc7d223fb7
Jehaechan <71350045+Jehaechan@users.noreply.github.com>
2022-07-12 03:56:53 +0900

    Update data.js

--- demoday/src/data.js
+++ demoday/src/data.js
@@ -6,10 +6,10 @@ https://www.iconfinder.com/Makoto_msk */
 export const storeProducts = [
   {
     id: 1,
-    title: "Google Pixel - Black",
+    title: "Google Pixel - RED",
     img: "img/product-1.png",
     price: 10,
-    company: "APPLE",
+    company: "GOOGLE",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
     inCart: false,
