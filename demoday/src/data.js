13c9e3ca69c7511e2e82164737f6fd2c98592754
Jehaechan <71350045+Jehaechan@users.noreply.github.com>
2022-07-12 03:59:43 +0900

    Update data.js

--- demoday/src/data.js
+++ demoday/src/data.js
@@ -6,7 +6,7 @@ https://www.iconfinder.com/Makoto_msk */
 export const storeProducts = [
   {
     id: 1,
-    title: "Google Pixel - RED",
+    title: "Google Pixel - BLACK",
     img: "img/product-1.png",
     price: 10,
     company: "GOOGLE",
